811362690d7d040d156182c598250426039aa837
cameron581 <cameron581@cox.net>
2014-05-01 14:34:25 -0500

    liquids weird naming system

--- lego.mk
+++ lego.mk
@@ -23,7 +23,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
 # tablet
-$(call inherit-product, vendor/lego/config/common_tablet.mk)
+$(call inherit-product, vendor/lego/config/common_full_tablet.mk)
 
 # enhanced NFC
 $(call inherit-product, vendor/lego/config/nfc_enhanced.mk)
